1713b0abfd304c17fd6e6c93c598432c2332b8d8
Pavlo Kulesha <pkulesha@lohika.com>
2021-10-06 13:18:46 +0300

    add prettier, htag, document, global styles

---
+++ pages/dist/_app.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+require("../styles/globals.css");
+function MyApp(_a) {
+    var Component = _a.Component, pageProps = _a.pageProps;
+    return (react_1["default"].createElement(react_1["default"].Fragment, null));
+}
