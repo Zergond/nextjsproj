45f5cec164ce4434a6dae77d4bf16ec252e77bc0
Pavlo Kulesha <pkulesha@lohika.com>
2022-02-02 11:41:30 +0200

    first component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Htag } from "../components";
+import { Button, Htag } from "../components";
 
 export default function Home(): JSX.Element {
-  const a = 1;
   return (
     <>
       <Htag tag="h1">text</Htag>
       <Htag tag="h2">text</Htag>
       <Htag tag="h3">text</Htag>
+      <Button appearance="primary">text</Button>
     </>
   );
 }
