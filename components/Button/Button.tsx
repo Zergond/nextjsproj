b43c17601d7a5753cf3457f8c9693372f6f1d392
Pavlo Kulesha <pkulesha@lohika.com>
2022-02-09 14:29:39 +0200

    config update

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,10 +2,13 @@ import { ButtonProps } from "./Button.props";
 import styles from "./Button.module.css";
 import cn from "classnames";
 
-export const Button = ({ appearance, children }: ButtonProps): JSX.Element => {
+export const Button = ({ appearance, children, className, ...props }: ButtonProps): JSX.Element => {
   return (
     <>
-      <button className={cn(styles.button, { [styles.primary]: appearance === "primary", [styles.ghost]: appearance === "ghost" })}>
+      <button
+        className={cn(styles.button, className, { [styles.primary]: appearance === "primary", [styles.ghost]: appearance === "ghost" })}
+        {...props}
+      >
         {children}
       </button>
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,9 @@ export default function Home(): JSX.Element {
       <Htag tag="h1">text</Htag>
       <Htag tag="h2">text</Htag>
       <Htag tag="h3">text</Htag>
-      <Button appearance="primary">text</Button>
+      <Button appearance="primary" className="button1111">
+        text
+      </Button>
     </>
   );
 }
